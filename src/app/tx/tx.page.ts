93629a25362b95bed9ab7db2f69c195c08764368
CodeMeen <alameenolumomi@yahoo.com>
2022-10-15 22:18:11 +0100

    commit

--- src/app/tx/tx.page.ts
+++ src/app/tx/tx.page.ts
@@ -1,97 +1,129 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { IonRouterOutlet } from '@ionic/angular';
-import { RouterService } from '../router.service';
-import { WalletsService } from '../wallets.service';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { IonRouterOutlet } from "@ionic/angular";
+import { RouterService } from "../router.service";
+import { WalletsService } from "../wallets.service";
+import { Browser } from "@capacitor/browser";
+import { Clipboard } from "@capacitor/clipboard";
+import { NotiService } from "../noti.service";
 
 @Component({
-  selector: 'app-tx',
-  templateUrl: './tx.page.html',
-  styleUrls: ['./tx.page.scss'],
-  encapsulation:ViewEncapsulation.None
+  selector: "app-tx",
+  templateUrl: "./tx.page.html",
+  styleUrls: ["./tx.page.scss"],
+  encapsulation: ViewEncapsulation.None,
 })
 export class TxPage implements OnInit {
+  txdata: any;
+  refinedTxData: any = {};
+  mytoken: any = {};
 
-  txdata:any
-  refinedTxData:any={}
-  mytoken:any={}
-
-  constructor(private route: ActivatedRoute,private routerOutlet: IonRouterOutlet,public router: RouterService,public wallet:WalletsService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private routerOutlet: IonRouterOutlet,
+    public router: RouterService,
+    public wallet: WalletsService, 
+    public noti: NotiService
+  ) {}
 
   async reducenumber(num, fixed) {
-
-    let string = await num.toString()
+    let string = await num.toString();
 
     if (!fixed) {
-
-        if (string.length <= 11) {
-            return string
-        } else {
-            let trimmedString = await string.slice(0, 11);
-            return trimmedString
-        }
-
+      if (string.length <= 11) {
+        return string;
+      } else {
+        let trimmedString = await string.slice(0, 11);
+        return trimmedString;
+      }
     } else {
-        if (string.length <= fixed) {
-            return string
-        } else {
-            let trimmedString = await string.slice(0, fixed);
-            return trimmedString
-        }
+      if (string.length <= fixed) {
+        return string;
+      } else {
+        let trimmedString = await string.slice(0, fixed);
+        return trimmedString;
+      }
     }
+  }
+  async refineTx(tx) {
+    this.refinedTxData = tx;
+    this.refinedTxData["refinedDate"] = this.timeConverter(tx.timeStamp);
+    let gasFee = this.getTxFee(tx.gasUsed, tx.gasPrice);
+    this.refinedTxData["gasFee"] = await this.reducenumber(gasFee, 7);
 
-}
-  async refineTx(tx){
-  this.refinedTxData=tx
-  this.refinedTxData['refinedDate']=this.timeConverter(tx.timeStamp)
-  let gasFee=this.getTxFee(tx.gasUsed,tx.gasPrice)
-  this.refinedTxData['gasFee']=await this.reducenumber(gasFee,7);
+    let gasFeeusd = await this.wallet.gasFeeUsd(
+      this.mytoken.name,
+      this.mytoken.type,
+      tx.gasFee
+    );
+    this.refinedTxData["gasFeeUsd"] = await this.reducenumber(gasFeeusd, 5);
 
-let gasFeeusd=await this.wallet.gasFeeUsd(this.mytoken.name,this.mytoken.type,tx.gasFee)
-  this.refinedTxData['gasFeeUsd']=await this.reducenumber(gasFeeusd,5);
+    let tokenvalueusd = await (tx.tokenvalue * this.mytoken.usdprice);
+    this.refinedTxData["tokenvalueusd"] = await this.reducenumber(
+      tokenvalueusd,
+      11
+    );
 
-  let tokenvalueusd=await (tx.tokenvalue * this.mytoken.usdprice)
-  this.refinedTxData['tokenvalueusd']=await this.reducenumber(tokenvalueusd,11)
-
-
-  console.log(this.refinedTxData)
-
+    console.log(this.refinedTxData);
   }
 
-  getTxFee(gasused,gasprice){
-  let data=gasused*gasprice
-  let newdat=data/1000000000000000000
-  return newdat
+  getTxFee(gasused, gasprice) {
+    let data = gasused * gasprice;
+    let newdat = data / 1000000000000000000;
+    return newdat;
   }
 
-  timeConverter(UNIX_timestamp){
-
+  timeConverter(UNIX_timestamp) {
     var a = new Date(UNIX_timestamp * 1000);
-    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
     var year = a.getFullYear();
     var month = months[a.getMonth()];
     var date = a.getDate();
     var hour = a.getHours();
     var min = a.getMinutes();
     var sec = a.getSeconds();
-    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+    var time =
+      date + " " + month + " " + year + " " + hour + ":" + min + ":" + sec;
     return time;
   }
 
-  async ngOnInit() {
+  async goToExplorer(hash) {
+    let network = await this.wallet.getCurrentNetworkName(),
+      mapurl;
 
-this.mytoken=await this.wallet.readViewData('mytoken')
-this.txdata=await this.wallet.readViewData('txdata')
+    if (network == "mainnet") {
+      mapurl = `https://etherscan.io/tx/${hash}`;
+    } else {
+      mapurl = `https://${network}.etherscan.io/tx/${hash}`;
+    }
 
-console.log(this.mytoken)
-
-if(!this.txdata || !this.mytoken){
-  this.router.goBack()
-}else{
-  await this.refineTx(this.txdata)
-}
-
-
+    await Browser.open({ url: mapurl });
   }
 
+
+  async ngOnInit() {
+    this.mytoken = await this.wallet.readViewData("mytoken");
+    this.txdata = await this.wallet.readViewData("txdata");
+
+    console.log(this.mytoken);
+
+    if (!this.txdata || !this.mytoken) {
+      this.router.goBack();
+    } else {
+      await this.refineTx(this.txdata);
+    }
+  }
 }
--- src/app/viewnft/viewnft.page.ts
+++ src/app/viewnft/viewnft.page.ts
@@ -1,113 +1,128 @@
-import { AfterContentChecked, ChangeDetectorRef, Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { RouterService } from '../router.service';
-import { WalletsService } from '../wallets.service';
+import {
+  AfterContentChecked,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+  ViewChild,
+  ViewEncapsulation,
+} from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { RouterService } from "../router.service";
+import { WalletsService } from "../wallets.service";
 import { EventsParams, SwiperComponent } from "swiper/angular";
 import SwiperCore, { SwiperOptions, Pagination, EffectCards } from "swiper";
-import { HttpClient } from '@angular/common/http';
-import { IonRouterOutlet } from '@ionic/angular';
+import { HttpClient } from "@angular/common/http";
+import { IonRouterOutlet } from "@ionic/angular";
+import { Browser } from "@capacitor/browser";
+import { Clipboard } from "@capacitor/clipboard";
+import { NotiService } from "../noti.service";
 
-SwiperCore.use([EffectCards,Pagination]);
+SwiperCore.use([EffectCards, Pagination]);
 
 @Component({
-  selector: 'app-viewnft',
-  templateUrl: './viewnft.page.html',
-  styleUrls: ['./viewnft.page.scss'],
-  encapsulation:ViewEncapsulation.None
+  selector: "app-viewnft",
+  templateUrl: "./viewnft.page.html",
+  styleUrls: ["./viewnft.page.scss"],
+  encapsulation: ViewEncapsulation.None,
 })
-export class ViewnftPage implements AfterContentChecked,OnInit{
+export class ViewnftPage implements AfterContentChecked, OnInit {
+  @ViewChild("swiper") swiper: SwiperComponent;
 
-  @ViewChild('swiper') swiper: SwiperComponent;
-
-  config: SwiperOptions= {
+  config: SwiperOptions = {
     slidesPerView: 1,
-    pagination:{
-      clickable:true,
-      type:'fraction'
+    pagination: {
+      clickable: true,
+      type: "fraction",
     },
-    effect: 'cards',
+    effect: "cards",
     cardsEffect: {
       // ...
     },
+  };
+
+  nfts: any[] = [];
+
+  currentNftIndex: any;
+  currentNft: any = {};
+  currentMetadata: any = {};
+  popUp: any = {
+    popopened: false,
+    height: "maxi",
+    messagetitle: "Send NFT",
+    message: "See NFT details and enter recipient address below",
+    imageurl: "",
+  };
+
+  nftToSend: any = {};
+
+  NftSendingData: any = {
+    recipient: "",
+  };
+
+  constructor(
+    public router: RouterService,
+    private route: ActivatedRoute,
+    public wallet: WalletsService,
+    private http: HttpClient,
+    private routerOutlet: IonRouterOutlet,
+    private cd: ChangeDetectorRef,
+    public noti: NotiService
+  ) {}
+
+  async copyStr(text) {
+    await Clipboard.write({
+      string: text,
+    });
+
+    this.noti.notify("success", "Copied!");
+  }
+
+  closePop() {
+    this.popUp = {
+      popopened: false,
+      height: true,
+      messagetitle: "",
+      message: "",
+      imageurl: "",
+    };
+  }
+
+  sendCurrentNft() {
+    let messagetitle = "Send NFT";
+    let message = "See NFT details and enter recipient address below";
+
+    this.popUp = {
+      popopened: true,
+      height: "maxi",
+      messagetitle: messagetitle,
+      message: message,
+      imageurl: this.currentNft.loaded_media,
     };
 
-
-  nfts:any[]=[]
-
-  currentNftIndex:any 
-  currentNft:any={}
-  currentMetadata:any={}
-  popUp:any={
-    'popopened':false,
-    'height':'maxi',
-    'messagetitle':'Send NFT',
-    'message':'See NFT details and enter recipient address below',
-    'imageurl':''
-  }
-
-  nftToSend:any={}
-
-  NftSendingData:any={
-    'recipient':''
-  }
-  
-
-  constructor(public router:RouterService,private route: ActivatedRoute,public wallet:WalletsService, private http: HttpClient,private routerOutlet: IonRouterOutlet,private cd: ChangeDetectorRef,) { }
-
-
-  closePop(){
-this.popUp={
-  'popopened':false,
-  'height':true,
-  'messagetitle':'',
-  'message':'',
-  'imageurl':''
-}
-  }
-
-  sendCurrentNft(){
-
-    let messagetitle='Send NFT'
-    let message='See NFT details and enter recipient address below'
-
-    this.popUp={
-      'popopened':true,
-      'height':'maxi',
-      'messagetitle':messagetitle,
-      'message':message,
-      'imageurl':this.currentNft.loaded_media
-    }
-
-    
-     
-    this.nftToSend=this.currentNft
-
+    this.nftToSend = this.currentNft;
   }
 
   shortAddr(string) {
+    let firstpart = string.slice(0, 7);
+    let lastpart = string.slice(-7);
 
-    let firstpart = string.slice(0, 7)
-    let lastpart = string.slice(-7)
+    let newstring = firstpart + "..." + lastpart;
 
-    let newstring = firstpart + '...' + lastpart
-
-    return newstring
-
-}
-
-  updatecurrindex(event){
-    this.currentNftIndex = this.swiper.swiperRef.activeIndex;
-    this.currentNft=this.nfts[this.currentNftIndex];
-    this.currentNft['shortAddress']=this.shortAddr(this.currentNft.token_address)
-    this.currentMetadata=JSON.parse(this.currentNft.metadata)
-
-
-if(!this.currentMetadata.name || this.currentMetadata.name==''){
-  this.currentMetadata['name']=this.currentNft.name
+    return newstring;
   }
-  
 
-    
+  updatecurrindex(event) {
+    this.currentNftIndex = this.swiper.swiperRef.activeIndex;
+    this.currentNft = this.nfts[this.currentNftIndex];
+    this.currentNft["shortAddress"] = this.shortAddr(
+      this.currentNft.token_address
+    );
+    this.currentMetadata = JSON.parse(this.currentNft.metadata);
+
+    if (!this.currentMetadata.name || this.currentMetadata.name == "") {
+      this.currentMetadata["name"] = this.currentNft.name;
+    }
+
     this.cd.detectChanges();
   }
 
@@ -117,103 +132,106 @@ if(!this.currentMetadata.name || this.currentMetadata.name==''){
     for (let index = 0; index < nfts.length; index++) {
       let nftindex = index;
       let eachnft = nfts[index];
-      let imgUrl = eachnft.raw_media; 
+      let imgUrl = eachnft.raw_media;
 
-      let newImgUrl=imgUrl.replace("ipfs://", "https://ipfs.io/ipfs/");
+      let newImgUrl = imgUrl.replace("ipfs://", "https://ipfs.io/ipfs/");
 
-      if(newImgUrl.search('data:image') >= 1){
-        eachnft['media_type']='image'
-        eachnft['loaded_media']=newImgUrl
-        eachnft['media_status']='loaded'
-      }else if(newImgUrl.search('data:video') >= 1){
-        eachnft['media_type']='video'
-        eachnft['loaded_media']=newImgUrl
-        eachnft['media_status']='loaded'
-      }else{
+      if (newImgUrl.search("data:image") >= 1) {
+        eachnft["media_type"] = "image";
+        eachnft["loaded_media"] = newImgUrl;
+        eachnft["media_status"] = "loaded";
+      } else if (newImgUrl.search("data:video") >= 1) {
+        eachnft["media_type"] = "video";
+        eachnft["loaded_media"] = newImgUrl;
+        eachnft["media_status"] = "loaded";
+      } else {
         this.http.get(newImgUrl, { responseType: "blob" }).subscribe(
           async (res) => {
             let blobdata: Blob = res;
 
-          
-            if( (blobdata.type == 'image/png' || blobdata.type == 'image/gif' ||  blobdata.type == 'image/jpeg' || blobdata.type == 'video/mp4') && blobdata.size <= 15000000 ){
-
-              
-           
-
-              let gendata=new Promise((resolve,reject)=>{
+            if (
+              (blobdata.type == "image/png" ||
+                blobdata.type == "image/gif" ||
+                blobdata.type == "image/jpeg" ||
+                blobdata.type == "video/mp4") &&
+              blobdata.size <= 15000000
+            ) {
+              let gendata = new Promise((resolve, reject) => {
                 const reader = new FileReader();
                 reader.onloadend = () => resolve(reader.result);
-  
-                reader.readAsDataURL(blobdata);
-              })
-  
-              let base64img=await gendata
 
-              if( blobdata.type == 'image/png' || blobdata.type == 'image/gif' ||  blobdata.type == 'image/jpeg' ){
-                eachnft['media_type']='image'
-              }else if(blobdata.type == 'video/mp4'){
-                eachnft['media_type']='video'
+                reader.readAsDataURL(blobdata);
+              });
+
+              let base64img = await gendata;
+
+              if (
+                blobdata.type == "image/png" ||
+                blobdata.type == "image/gif" ||
+                blobdata.type == "image/jpeg"
+              ) {
+                eachnft["media_type"] = "image";
+              } else if (blobdata.type == "video/mp4") {
+                eachnft["media_type"] = "video";
               }
 
-              eachnft['loaded_media']=base64img
-              eachnft['media_status']='loaded'
-
-
+              eachnft["loaded_media"] = base64img;
+              eachnft["media_status"] = "loaded";
             }
-
           },
 
           (err) => {
-            eachnft['media_status']='error'
-            eachnft['loaded_media']='../../assets/images/offline.svg'
+            eachnft["media_status"] = "error";
+            eachnft["loaded_media"] = "../../assets/images/offline.svg";
             console.log(err);
-            
           }
         );
       }
-
-     
-    
-
-     
-
-      
     }
+  }
+
+  async goToOpensea(token_addr,token_id) {
+    let network=await this.wallet.getCurrentNetworkName(), 
+    mapurl;
+
+    if(network != "mainnet"){
+      mapurl=`https://testnets.opensea.io/assets/${token_addr}/${token_id}`
+    }else{
+      mapurl=`https://opensea.io/assets/${token_addr}/${token_id}`
+    }
+
+
+
+    await Browser.open({ url: mapurl });
   }
 
   ngAfterContentChecked() {
-
-    if(this.swiper){
-    this.swiper.updateSwiper({});
+    if (this.swiper) {
+      this.swiper.updateSwiper({});
     }
-    
-    }
-
-
-
- async ngOnInit() {
-  this.routerOutlet.swipeGesture = true;
-    const routeParams = this.route.snapshot.paramMap;
-    let nftaddr=routeParams.get('nftaddr');
-
-await this.wallet.loadNft(nftaddr).then((data)=>{
-
-this.nfts=data
-this.currentNft=this.nfts[0]
-this.currentNft['shortAddress']=this.shortAddr(this.currentNft.token_address)
-this.currentMetadata=JSON.parse(this.currentNft.metadata)
-
-if(!this.currentMetadata.name || this.currentMetadata.name==''){
-this.currentMetadata['name']=this.currentNft.name
-}
-
-this.loadNftImgs()
-},
-(error)=>{
-
-})
-
-    
   }
 
+  async ngOnInit() {
+    this.routerOutlet.swipeGesture = true;
+    const routeParams = this.route.snapshot.paramMap;
+    let nftaddr = routeParams.get("nftaddr");
+
+    await this.wallet.loadNft(nftaddr).then(
+      (data) => {
+        this.nfts = data;
+        this.currentNft = this.nfts[0];
+        this.currentNft["shortAddress"] = this.shortAddr(
+          this.currentNft.token_address
+        );
+        this.currentMetadata = JSON.parse(this.currentNft.metadata);
+
+        if (!this.currentMetadata.name || this.currentMetadata.name == "") {
+          this.currentMetadata["name"] = this.currentNft.name;
+        }
+
+        this.loadNftImgs();
+      },
+      (error) => {}
+    );
+  }
 }
