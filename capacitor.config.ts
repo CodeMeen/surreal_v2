5656d526a2c5013091b13edea3e594dd956d1469
Ameen <alameenolumomi@yahoo.com>
2022-12-08 14:37:56 +0100

    commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,16 @@
 import { CapacitorConfig } from '@capacitor/cli';
+import { KeyboardResize } from '@capacitor/keyboard';
 
 const config: CapacitorConfig = {
   appId: 'io.surreal.wallet',
   appName: 'Surreal Wallet',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    Keyboard: {
+      resize: KeyboardResize.None,
+    },
+  }
 };
 
 export default config;
